498a2d0a526813422d7ff561f9cb08363b19d920
h.yw <rumengzhenxing@gmail.com>
2019-04-30 11:46:41 +0800

    1.0.3

--- app/src/main/java/com/love/shortcut/QuickSettingService.java
+++ app/src/main/java/com/love/shortcut/QuickSettingService.java
@@ -1,8 +1,12 @@
 package com.love.shortcut;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 import android.service.quicksettings.TileService;
+
+import java.lang.reflect.Method;
 
 import static com.love.shortcut.RootUtils.runRootCommand;
 
@@ -30,6 +34,7 @@ public class QuickSettingService extends TileService {
     @Override
     public void onClick() {
         runRootCommand(" am start -n com.miui.voiceassist/com.xiaomi.voiceassistant.AiSettings.AiShortcutActivity -a action.intent.action.VIEW");
+        collapseStatusBar(getBaseContext());
     }
 
     @Override
@@ -40,5 +45,25 @@ public class QuickSettingService extends TileService {
     @Override
     public void onStopListening() {
 
+    }
+
+    /**
+     * 收起通知栏
+     *
+     * @param context
+     */
+    private void collapseStatusBar(Context context) {
+        try {
+            @SuppressLint("WrongConstant") Object statusBarManager = context.getSystemService("statusbar");
+            Method collapse;
+            if (Build.VERSION.SDK_INT <= 16) {
+                collapse = statusBarManager.getClass().getMethod("collapse");
+            } else {
+                collapse = statusBarManager.getClass().getMethod("collapsePanels");
+            }
+            collapse.invoke(statusBarManager);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
